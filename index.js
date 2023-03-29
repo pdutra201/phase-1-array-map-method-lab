2e23bca98548ec34b5c126f9e45c4a82d1bcef5f
philip dutra <monsterphilip10@gmail.com>
2023-03-29 14:05:24 -0400

    updated index.js

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = () => { 
+  
+  return tutorials.map(function (str) {
+    return str.split(" ").map(function(word){
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ");
+  });
 }
